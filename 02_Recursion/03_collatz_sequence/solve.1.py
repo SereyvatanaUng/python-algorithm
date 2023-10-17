e63a3589abfed720e731b7f8d44dc9063e198e8c
SereyvatanaUng <sereyvatanau@gmail.com>
2023-10-17 10:46:31 +0700

    collatz sequence

---
+++ 02_Recursion/03_collatz_sequence/solve.1.py
@@ -0,0 +1,13 @@
+def collatz(n):
+    if n == 1:
+        return [1]
+    if n % 2 == 0:
+        return [n] + collatz(n // 2)
+    else:  # n % 2 == 1
+        return [n] + collatz(n * 3 + 1)
+
+
+N = int(input())
+S = collatz(N)
+print(len(S))
+print(" ".join(map(str, S)))
