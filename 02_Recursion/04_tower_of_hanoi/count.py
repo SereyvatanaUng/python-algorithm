c49f5f8f8d91edf3d5d7452f9a3de3a77e3b6239
SereyvatanaUng <sereyvatanau@gmail.com>
2023-10-17 10:46:52 +0700

    tower of hanoi

---
+++ 02_Recursion/04_tower_of_hanoi/count.py
@@ -0,0 +1,14 @@
+def hanoi(n, src, via, dst):
+    global cnt
+    if n == 1:
+        cnt += 1  # print(f"{src} -> {dst}")
+    else:
+        hanoi(n - 1, src, dst, via)
+        hanoi(1, src, via, dst)
+        hanoi(n - 1, via, src, dst)
+
+
+N = int(input())
+cnt = 0
+hanoi(N, 'A', 'B', 'C')
+print(cnt)
---
+++ 02_Recursion/04_tower_of_hanoi/solve.1.py
@@ -0,0 +1,30 @@
+# def hanoi(n, src, via, dst):
+#     print(n, src, via, dst)
+
+#     if n == 1:
+#         print(f"{src} -> {dst}\n")
+
+#     else:
+#         hanoi(n - 1, src, dst, via)
+#         hanoi(1, src, via, dst)
+#         hanoi(n - 1, via, src, dst)
+
+# N = int(input())
+# hanoi(N, 'A', 'B', 'C')
+
+######################
+# DEBUG
+def hanoi(n, src, via, dst, x):
+
+    if n == 1:
+        print(n, src, via, dst, x)
+        print(f"{src} -> {dst}\n")
+
+    else:
+        hanoi(n - 1, src, dst, via, '	else 1')
+        hanoi(1, src, via, dst, '	else 2')
+        hanoi(n - 1, via, src, dst, '	else 3')
+
+
+N = int(input())
+hanoi(N, 'A', 'B', 'C', '	initial')
