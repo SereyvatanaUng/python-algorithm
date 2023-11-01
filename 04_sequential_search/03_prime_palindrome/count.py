d62c717db724a88c6ab168e1aa58c705bb9633d0
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-01 08:56:12 +0700

    4.3 Prime Palindrome

---
+++ 04_sequential_search/03_prime_palindrome/count.py
@@ -0,0 +1,35 @@
+import time
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_palindrome(n):
+    s = str(n)
+    r = s[::-1]
+    return int(s) == int(r)
+
+
+def solve(n, m):
+    cnt1 = 0
+    for i in range(n, m + 1):
+        if is_palindrome(i):
+            cnt1 += 1
+    cnt2 = 0
+    for i in range(n, m+1):
+        if is_prime(i):
+            cnt2 += 1
+    return cnt1, cnt2
+
+
+N, M = map(int, input().split())
+# start = time.time()
+print(solve(N, M))
+# end = time.time()
+# print(f'solve() elapsed time: {end - start}')
