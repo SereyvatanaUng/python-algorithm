86d460e35e9d30e5811a2ef7e5acd164510b9ee2
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-02 20:02:39 +0700

    Count of Prime Factors

---
+++ 04_sequential_search/06_count_of_prime_factors/solve.1.py
@@ -0,0 +1,21 @@
+def count_factor(n):
+    cnt = 0
+    for i in range(2, int(n ** 0.5) + 1):
+        while n % i == 0:
+            cnt += 1
+            n = n // i
+
+    if n != 1:
+        cnt += 1
+    return cnt
+
+
+def solve(n):
+    maxcnt = 0
+    for i in range(2, n + 1):
+        maxcnt = max(maxcnt, count_factor(i))
+    print(maxcnt)
+
+
+N = int(input())
+solve(N)
