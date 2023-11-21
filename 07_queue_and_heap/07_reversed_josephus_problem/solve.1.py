46642900ecbb05b6ebf308b7efeb71fa658989fd
SereyvatanaUng <sereyvatanau@gmail.com>
2023-11-21 09:58:27 +0700

    7.7 Reversed Josephus Problem

---
+++ 07_queue_and_heap/07_reversed_josephus_problem/solve.1.py
@@ -0,0 +1,15 @@
+def solve(n, k, m):
+    queue = [i for i in range(1, n + 1)]
+    j, r = 0, 0
+    while queue:
+        if (r // m) % 2 == 0:
+            j = (j + k - 1) % len(queue)
+        else:
+            j = (j - k) % len(queue)
+        r += 1
+        print(queue.pop(j), end=" ")
+    print()
+
+
+N, K, M = map(int, input().split())
+solve(N, K, M)
