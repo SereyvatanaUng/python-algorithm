ba527b4c05a9d355689f611939f3548e2f62ea9d
SereyvatanaUng <sereyvatanau@gmail.com>
2023-12-19 09:12:55 +0700

    12.3 Optimal path on a triangle

---
+++ 12_dynamic_programming_2/03_optimal_path_on_a_triangle/solve.2.py
@@ -0,0 +1,25 @@
+# Find far right path
+
+def solve(n, t):
+    dp = [t[i][:] for i in range(n)]
+    path = [[-1]*(i+1) for i in range(n)]
+    for i in range(n-2, -1, -1):
+        for j in range(i+1):
+            if dp[i+1][j] > dp[i+1][j+1]:
+                dp[i][j] += dp[i+1][j]
+                path[i][j] = 0
+            else:
+                dp[i][j] += dp[i+1][j+1]
+                path[i][j] = 1
+    return dp[0][0], path
+
+
+N = int(input())
+T = [list(map(int, input().split())) for _ in range(N)]
+opt, path = solve(N, T)
+print(opt)
+j = 0
+for i in range(N):
+    print(T[i][j], end=" ")
+    j += path[i][j]
+print()
